39e923a31efaad1e8c78be65a9d57ce0d2666c3d
Toni <Toni@DESKTOP-EMNU40J>
2017-11-15 07:29:26 +0200

    Labrat

---
+++ Pintaala/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pintaala
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int l;
+                int h;
+                int w;
+                txbVirhe.Text = "";
+                if (int.TryParse(txtLeveys.Text, out l) && int.TryParse(txtKorkeus.Text, out h))
+                {
+                    if (txtKarmi.Text == "")
+                    {
+                        w = 45;
+                    }
+                    else
+                    {
+                        w = int.Parse(txtKarmi.Text);
+                    }
+                    if (l >= 300 && l <= 3000 && h >= 300 && h <= 3000)
+                    {
+                        Laske(l, h, w);
+                    }
+                    else
+                    {
+                        txbVirhe.Text = "Korkeus tai leveys on liian suuri.";
+                        txbPA.Text = "";
+                        txbLPA.Text = "";
+                        txbKP.Text = "";
+                    }
+                }
+                else
+                {
+                    txbPA.Text = "Jotain meni väärin";
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                txbVirhe.Text = ex.Message;
+            }
+        }
+        public void Laske(int l, int h, int w)
+        {
+            try
+            {
+                int pa = (l * h) / 10;
+                int lpa = ((l - (w * 2)) * (h - (w * 2))) / 10;
+                int kpiiri = ((l * 2) + (h * 2)) / 10;
+                txbPA.Text = pa.ToString() + " cm^2";
+                txbLPA.Text = lpa.ToString() + " cm^2";
+                txbKP.Text = kpiiri.ToString() + " cm";
+                ikkuna.Width = l / 10;
+                ikkuna.Height = h / 10;
+                ikkuna.StrokeThickness = w / 10;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ Valuuttalaskuri/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Valuuttalaskuri
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void txtLuku_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                double euro;
+                double dol;
+                if (cmbOstan.Text == usd.Content.ToString())
+                {
+                    txbValuuttatiedot1.Text = usd.Content + " myyntikurssi 0.8997";
+                    txbValuutta.Text = eur.Content.ToString();
+                    if (double.TryParse(txtLuku.Text, out dol))
+                    {
+                        euro = dol * 0.8997;
+                        txbMaksan.Text = euro.ToString("0.00");
+                        txbValuuttatiedot2.Text = dol.ToString() + " euroa on vastaa " + (dol / 0.8997).ToString("0.00") + " USD";
+                    }
+                    else
+                    {
+                        txbMaksan.Text = "Syötä numeroja";
+                    }
+                }
+                else
+                {
+                    txbValuuttatiedot1.Text = eur.Content + " myyntikurssi 1.1115";
+                    txbValuutta.Text = usd.Content.ToString();
+                    if (double.TryParse(txtLuku.Text, out euro))
+                    {
+                        dol = euro * 1.1115;
+                        txbMaksan.Text = dol.ToString("0.00");
+                        txbValuuttatiedot2.Text = euro.ToString() + " USD vastaa " + (euro / 1.1115).ToString("0.00") + " euroa";
+                    }
+                    else
+                    {
+                        txbMaksan.Text = "Syötä numeroja";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                txbMaksan.Text = ex.Message;
+            }
+        }
+    }
+}
